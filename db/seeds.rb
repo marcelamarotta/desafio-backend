9152bbc9743aecae946660a6bd33e86d6c569bda
marcela marotta <marcelamarotta@gmail.com>
2021-02-15 09:14:12 -0300

    seed ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,54 @@
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 require 'csv'
 
 csv_options = { col_sep: ';', encoding: "bom|utf-8", headers: :first_row }
 filepath = 'db/Ano-2020.csv'
 uf = "MG"
-n = 0
+csv_data = []
+lala = 0
 CSV.foreach(filepath, csv_options) do |row|
   if row['sgUF'] == uf
-    if Congressman.exists?( name: row['txNomeParlamentar'] )
-      congressman = Congressman.find_by name: row['txNomeParlamentar']
-    else
-      congressman = Congressman.create( uf: uf, name: row['txNomeParlamentar'] )
-    end
-    expense = Expense.create( date: row['datEmissao'],
-                            provider: row['txtFornecedor'],
-                            amount: row['vlrLiquido'],
-                            url: row['urlDocumento'],
-                            congressman_id: congressman[:id]
-                            )
+    data = { uf: uf,
+             name: row['txNomeParlamentar'],
+             date: row['datEmissao'],
+             provider: row['txtFornecedor'],
+             amount: row['vlrLiquido'],
+             url: row['urlDocumento']
+    }
+    csv_data << data
+    lala += 1
+    p lala
+  end
+end
+congressmen = []
+expenses = []
+id = 0
+n = 0
+csv_data.each do |data|
+  if congressmen.any? { |element| element[:name] == data[:name] }
+    congressman_id = congressmen.find {|congressman| congressman[:name] == data[:name] }[:id]
+  else 
+    id += 1
+    congressman = { id: id,
+                    uf: uf,
+                    name: data[:name],
+                    created_at: Time.now.utc,
+                    updated_at: Time.now.utc
+    }
+    congressmen << congressman
+    congressman_id = id
   end
   n += 1
-  puts n
+  expense = { id: n,
+              date: data[:date],
+              provider: data[:provider],
+              amount: data[:amount],
+              url: data[:url],
+              congressman_id: congressman_id,
+              created_at: Time.now.utc,
+              updated_at: Time.now.utc
+  }
+  expenses << expense
 end
+
+Congressman.insert_all(congressmen)
+Expense.insert_all(expenses)
